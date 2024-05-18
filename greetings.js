d995ac42f52fbadaeb89ffa6edaeb00aca4e6d96
gorgeous-lady1 <167902564+gorgeous-lady1@users.noreply.github.com>
2024-05-18 15:58:23 -0700

    Add files via upload

---
+++ greetings.js
@@ -0,0 +1,6 @@
+"use strict";
+let names = ['Shayan', 'Hassan', 'Aslam', 'Majid', 'Shoaib', 'Faisal'];
+const message = 'Welcome Home: ';
+for (let i = 0; i < names.length; i++) {
+    console.log(message + names[i]);
+}
